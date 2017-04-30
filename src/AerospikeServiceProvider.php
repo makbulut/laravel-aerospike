06f212167563023fed4b722b887b9a9aa4f889d4
Mehmet AKBULUT <mehmetakbulut.mail@gmail.com>
2017-04-30 20:27:27 +0300

    Update AerospikeServiceProvider.php
    fix

--- src/AerospikeServiceProvider.php
+++ src/AerospikeServiceProvider.php
@@ -8,17 +8,11 @@
 
 namespace Makbulut\Aerospike;
 
-use Cache;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\ServiceProvider;
 
 class AerospikeServiceProvider extends ServiceProvider
 {
-    /**
-     * Indicates if loading of the provider is deferred.
-     *
-     * @var bool
-     */
-    protected $defer = true;
 
     /**
      * Register the service provider.
@@ -36,45 +30,20 @@ class AerospikeServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        $configPath = __DIR__ . '/../config/aerospike.php';
-        $this->publishes([$configPath => config_path('aerospike.php')], 'config');
-
+        $this->mergeConfigFrom(__DIR__ . '/../config/aerospike.php', 'cache.stores.aerospike');
 
         Cache::extend('aerospike', function ($app) {
 
             $config = $app['config'];
 
-            if ($config['aerospike.servers']) {
-                $config = $config['aerospike.servers'];
-            } else {
-                $config = ["hosts" => [["addr" => "localhost", "port" => 3000]]];
-            }
+            $server = $config['cache.stores.aerospike.servers'];
 
-            $aerospike = new \Aerospike($config);
+            $aerospike = new \Aerospike($server);
 
-            $store = new AerospikeStore($aerospike, $this->getPrefix($config), $config['namespace']);
+            $store = new AerospikeStore($aerospike, $config['cache.prefix'], $config['cache.stores.aerospike.namespace']);
 
             return Cache::repository($store);
         });
     }
 
-
-    /**
-     * Get the services provided by the provider.
-     *
-     * @return array
-     */
-    public function provides()
-    {
-        return array('aerospike');
-    }
-
-    /**
-     * @param array $config
-     * @return string
-     */
-    protected function getPrefix(array $config)
-    {
-        return array_get($config, 'prefix') ?: $this->app['config']['cache.prefix'];
-    }
 }
